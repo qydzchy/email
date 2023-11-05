f7a8e4088bd1d5a2212cb9ee964de394eab31be8
w0r1d-peace <278882047@qq.com>
2023-11-05 21:50:06 +0800

    客户表结构更新。

--- sql/customer.sql
+++ sql/customer.sql
@@ -414,23 +414,30 @@ CREATE TABLE `customer_publicleads_claim_limit`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `customer_publicleads_groups`;
 CREATE TABLE `customer_publicleads_groups`  (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '分组名称',
-  `user_ids` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '分组成员ID（逗号拼接）',
-  `default_group_flag` tinyint(1) NOT NULL COMMENT '默认分组 0.否 1.是 ',
-  `del_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' COMMENT '删除标志(0代表存在2代表删除)',
-  `create_id` bigint(20) NULL DEFAULT NULL COMMENT '创建者ID',
-  `create_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '创建者',
-  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `update_id` bigint(20) NULL DEFAULT NULL COMMENT '更新者ID',
-  `update_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '更新者',
-  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`) USING BTREE
+`id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+`name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '分组名称',
+`default_group_flag` tinyint(1) NOT NULL COMMENT '默认分组 0.否 1.是 ',
+`del_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' COMMENT '删除标志(0代表存在2代表删除)',
+`create_id` bigint(20) NULL DEFAULT NULL COMMENT '创建者ID',
+`create_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '创建者',
+`create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+`update_id` bigint(20) NULL DEFAULT NULL COMMENT '更新者ID',
+`update_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '更新者',
+`update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '公海分组表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of customer_publicleads_groups
+-- Table structure for customer_publicleads_groups_user
 -- ----------------------------
+DROP TABLE IF EXISTS `customer_publicleads_groups_user`;
+CREATE TABLE `customer_publicleads_groups_user`  (
+`id` bigint(20) NOT NULL COMMENT '主键',
+`publicleads_groups_id` bigint(20) NULL DEFAULT NULL COMMENT '公海分组',
+`user_id` bigint(20) NULL DEFAULT NULL COMMENT '分组成员',
+PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '公海分组成员' ROW_FORMAT = Dynamic;
+
 
 -- ----------------------------
 -- Table structure for customer_publicleads_reason
