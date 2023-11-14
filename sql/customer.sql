d398af1451806221327f8a487314bd727f0657fd
278882047@qq.com <tjm19960701>
2023-11-14 08:21:25 +0800

    fix.

--- sql/customer.sql
+++ sql/customer.sql
@@ -640,8 +640,15 @@ CREATE TABLE `customer_customer_follow_up_personnel`  (
 `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
 `customer_id` bigint(20) NOT NULL COMMENT '客户ID',
 `user_id` bigint(20) NOT NULL COMMENT '跟进人ID',
+`del_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '0' COMMENT '删除标志(0代表存在2代表删除)',
+`create_id` bigint(20) NULL DEFAULT NULL COMMENT '创建者ID',
+`create_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '创建者',
+`create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+`update_id` bigint(20) NULL DEFAULT NULL COMMENT '更新者ID',
+`update_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '更新者',
+`update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
 PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '客户跟进人' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '客户跟进人' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for customer_customer_follow_up_records
