805cf76cde6a49db0bb95612b9dd3c73ce81111c
Tim署 <partnersudo@qq.com>
2023-12-26 00:14:23 +0800

    fix: 处理打包问题

--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -4,6 +4,8 @@ const path = require('path')
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
+const baseUrl = 'http://42.194.167.180:8080'
+// const baseUrl = 'http://localhost:8080'
 
 const CompressionPlugin = require('compression-webpack-plugin')
 
@@ -35,7 +37,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:8080`,
+        target: baseUrl,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
@@ -90,52 +92,54 @@ module.exports = {
       })
       .end()
     config.module.rule('pdf')
-        .test(/\.(pdf)(\?.*)?$/)
-        .use('file-loader')
-        .loader('file-loader')
-        .options({
-          name: 'pdf/[name].[hash:8].[ext]'
-        })
+      .test(/\.(pdf)(\?.*)?$/)
+      .use('file-loader')
+      .loader('file-loader')
+      .options({
+        name: 'pdf/[name].[hash:8].[ext]'
+      })
 
     config.when(process.env.NODE_ENV !== 'development', config => {
-          config
-            .plugin('ScriptExtHtmlWebpackPlugin')
-            .after('html')
-            .use('script-ext-html-webpack-plugin', [{
-            // `runtime` must same as runtimeChunk name. default is `runtime`
-              inline: /runtime\..*\.js$/
-            }])
-            .end()
+      config
+        .plugin('ScriptExtHtmlWebpackPlugin')
+        .after('html')
+        .use('script-ext-html-webpack-plugin', [{
+          // `runtime` must same as runtimeChunk name. default is `runtime`
+          inline: /runtime\..*\.js$/
+        }])
+        .end()
 
-          config.optimization.splitChunks({
-            chunks: 'all',
-            cacheGroups: {
-              libs: {
-                name: 'chunk-libs',
-                test: /[\\/]node_modules[\\/]/,
-                priority: 10,
-                chunks: 'initial' // only package third parties that are initially dependent
-              },
-              elementUI: {
-                name: 'chunk-elementUI', // split elementUI into a single package
-                test: /[\\/]node_modules[\\/]_?element-ui(.*)/, // in order to adapt to cnpm
-                priority: 20 // the weight needs to be larger than libs and app or it will be packaged into libs or app
-              },
-              commons: {
-                name: 'chunk-commons',
-                test: resolve('src/components'), // can customize your rules
-                minChunks: 3, //  minimum common number
-                priority: 5,
-                reuseExistingChunk: true
-              }
-            }
-          })
-
-          config.optimization.runtimeChunk('single'),
-          {
-             from: path.resolve(__dirname, './public/robots.txt'), //防爬虫文件
-             to: './' //到根目录下
+      config.optimization.splitChunks({
+        chunks: 'all',
+        cacheGroups: {
+          libs: {
+            name: 'chunk-libs',
+            test: /[\\/]node_modules[\\/]/,
+            priority: 10,
+            chunks: 'initial' // only package third parties that are initially dependent
+          },
+          elementUI: {
+            name: 'chunk-elementUI', // split elementUI into a single package
+            test: /[\\/]node_modules[\\/]_?element-ui(.*)/, // in order to adapt to cnpm
+            priority: 20 // the weight needs to be larger than libs and app or it will be packaged into libs or app
+          },
+          commons: {
+            name: 'chunk-commons',
+            test: resolve('src/components'), // can customize your rules
+            minChunks: 3, //  minimum common number
+            priority: 5,
+            reuseExistingChunk: true
           }
+        }
+      })
+
+      config.optimization.runtimeChunk('single'),
+      {
+        from: path.resolve(__dirname, './public/robots.txt'), //防爬虫文件
+        to: './' //到根目录下
+      }
+
+      config.plugins.delete('optimize-css')
     })
   }
 }
