068c7ae23a933126629e9cda622b45f7e077337a
w0r1d-peace <278882047@qq.com>
2024-01-14 10:52:52 +0800

    优化邮件列表

--- ruoyi-email/src/main/java/com/ruoyi/email/service/impl/TaskEmailServiceImpl.java
+++ ruoyi-email/src/main/java/com/ruoyi/email/service/impl/TaskEmailServiceImpl.java
@@ -193,8 +193,6 @@ public class TaskEmailServiceImpl implements ITaskEmailService {
             statusList = Arrays.asList(TaskExecutionStatusEnum.NOT_STARTED.getStatus(), TaskExecutionStatusEnum.IN_PROGRESS.getStatus(), TaskExecutionStatusEnum.FAILURE.getStatus());
         }
 
-        folderId = folderId != null ? folderId : -1L;
-
         int count = taskEmailMapper.count(taskIdList, type, readFlag, pendingFlag, spamFlag, delFlag, traceFlag, fixedFlag, attachmentFlag, customerFlag, folderId, labelId, statusList);
         if (count <= 0) {
             return Pair.of(0, new ArrayList<>());
