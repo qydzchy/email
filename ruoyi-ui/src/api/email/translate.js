2758ce77152cb8da49431ed58bedbd24c6495a7d
Tim署 <partnersudo@qq.com>
2024-01-22 09:23:10 +0800

    style: adjust

--- ruoyi-ui/src/api/email/translate.js
+++ ruoyi-ui/src/api/email/translate.js
@@ -1,10 +1,10 @@
 import request from '@/utils/request'
 
 // 查询模板类型列表
-export function translate(query) {
+export function translate(data) {
     return request({
         url: '/email/info/translate',
-        method: 'get',
-        params: query
+        method: 'post',
+        data
     })
 }
