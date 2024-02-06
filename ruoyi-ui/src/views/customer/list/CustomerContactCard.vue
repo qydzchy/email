9ab5c7b3be37160ac29248e5af48731b7903a4f1
Tim署 <partnersudo@qq.com>
2024-02-06 17:33:54 +0800

    fix: bug

--- ruoyi-ui/src/views/customer/list/CustomerContactCard.vue
+++ ruoyi-ui/src/views/customer/list/CustomerContactCard.vue
@@ -2,17 +2,17 @@
   <div class="contact-card">
     <div class="fs-16 my-10">联系人{{ formList.length }}</div>
     <div>
-      <div class="contact-card-ul" v-for="(item,index) in formList" :key="index">
+      <div class="contact-card-ul" v-for="(item, index) in formList" :key="index">
         <div class="flex-middle space-between">
           <div class="card-title">
             <span>{{ item.nickName || `联系人${index + 1}` }}</span>
-            <span v-if="index===0" class="pl-6">
-               <svg-icon icon-class="important"/>
+            <span v-if="index === 0" class="pl-6">
+              <svg-icon icon-class="important" />
             </span>
           </div>
-          <div v-if="index!==0">
+          <div v-if="index !== 0">
             <el-tooltip placement="top" content="设置为主联系人">
-              <svg-icon class="pointer" icon-class="important-gray" @click="onSortContactForm(item.contactId)"/>
+              <svg-icon class="pointer" icon-class="important-gray" @click="onSortContactForm(item.contactId)" />
             </el-tooltip>
             <el-tooltip placement="top" content="删除">
               <i class="el-icon-delete pl-10 pointer" @click="onDelContactForm(item.contactId)"></i>
@@ -22,32 +22,31 @@
 
         <el-form :ref="`contactRef-${index}`" :model="item" :rules="contactRule">
           <el-form-item label="昵称" prop="nickName">
-            <el-input v-model="item.nickName" placeholder="请输入"/>
+            <el-input v-model="item.nickName" placeholder="请输入" />
           </el-form-item>
           <el-form-item label="邮箱">
-            <el-input v-model="item.email" placeholder="请输入"/>
+            <el-input v-model="item.email" placeholder="请输入" />
           </el-form-item>
-          <el-form-item label="社交平台"/>
-          <el-form-item class="special-item" v-for="(platform,idxP) in item.socialPlatform" :key="`platform-${idxP}`">
+          <el-form-item label="社交平台" />
+          <el-form-item class="special-item" v-for="(platform, idxP) in item.socialPlatform" :key="`platform-${idxP}`">
             <el-row type="flex" :gutter="4">
               <el-col :span="8">
                 <el-select v-model="platform.type" placeholder="社交平台">
-                  <el-option v-for="(opt,index) in platformOption" :key="index" :label="opt.label"
-                             :value="opt.value">
+                  <el-option v-for="(opt, index) in platformOption" :key="index" :label="opt.label" :value="opt.value">
                     <span>
-                      <svg-icon :icon-class="opt.svg"/>
+                      <svg-icon :icon-class="opt.svg" />
                       {{ opt.label }}
                     </span>
                   </el-option>
                 </el-select>
               </el-col>
               <el-col :span="13">
-                <el-input v-model="platform.account" placeholder="请输入"/>
+                <el-input v-model="platform.account" placeholder="请输入" />
               </el-col>
               <el-col :span="3" class="flex-middle">
                 <el-tooltip placement="top" content="删除">
-                  <i v-if="idxP!==0" class="fs-20 pl-6 el-icon-remove-outline pointer"
-                     @click="onReduce(platform.id,'socialPlatform')"></i>
+                  <i v-if="idxP !== 0" class="fs-20 pl-6 el-icon-remove-outline pointer"
+                    @click="onReduce(item.contactId, platform.id, 'socialPlatform')"></i>
                 </el-tooltip>
                 <el-tooltip placement="top" content="添加">
                   <i class="fs-20 pl-6 el-icon-circle-plus-outline pointer" @click="onAdd('socialPlatform')"></i>
@@ -55,22 +54,22 @@
               </el-col>
             </el-row>
           </el-form-item>
-          <el-form-item label="联系电话"/>
-          <el-form-item class="special-item" v-for="(contact,idxC) in item.phone" :key="`contact-${idxC}`">
+          <el-form-item label="联系电话" />
+          <el-form-item class="special-item" v-for="(contact, idxC) in item.phone" :key="`contact-${idxC}`">
             <el-row type="flex" :gutter="4">
               <el-col :span="8">
                 <el-select v-model="contact.phone_prefix" placeholder="电话区号" filterable clearable>
-                  <el-option v-for="(prefix,index) in phonePrefixList" :key="index" :label="prefix.label"
-                             :value="prefix.value"></el-option>
+                  <el-option v-for="(prefix, index) in phonePrefixList" :key="index" :label="prefix.label"
+                    :value="prefix.value"></el-option>
                 </el-select>
               </el-col>
               <el-col :span="13">
-                <el-input v-model="contact.phone" type="number" placeholder="请输入"/>
+                <el-input v-model="contact.phone" type="number" placeholder="请输入" />
               </el-col>
               <el-col :span="3" class="flex-middle">
                 <el-tooltip placement="top" content="删除">
-                  <i v-if="idxC!==0" class="fs-20 pl-6 el-icon-remove-outline pointer"
-                     @click="onReduce(contact.id,'phone')"></i>
+                  <i v-if="idxC !== 0" class="fs-20 pl-6 el-icon-remove-outline pointer"
+                    @click="onReduce(item.contactId, contact.id, 'phone')"></i>
                 </el-tooltip>
                 <el-tooltip placement="top" content="添加">
                   <i class="fs-20 pl-6 el-icon-circle-plus-outline pointer" @click="onAdd('phone')"></i>
@@ -82,8 +81,7 @@
           <el-row v-if="item.show">
             <el-form-item label="职级">
               <el-select v-model="item.rank">
-                <el-option v-for="(opt,index) in rankOption" :key="index" :label="opt.label"
-                           :value="opt.value">
+                <el-option v-for="(opt, index) in rankOption" :key="index" :label="opt.label" :value="opt.value">
                 </el-option>
               </el-select>
             </el-form-item>
@@ -96,19 +94,15 @@
             <el-form-item label="性别" style="width: 210px;">
               <el-row>
                 <el-radio-group v-model="item.sex">
-                  <el-radio v-for="(sexItem,index) in sexRadio" :key="index" :label="sexItem.value">
+                  <el-radio v-for="(sexItem, index) in sexRadio" :key="index" :label="sexItem.value">
                     {{ sexItem.label }}
                   </el-radio>
                 </el-radio-group>
               </el-row>
             </el-form-item>
             <el-form-item label="头像/名片">
-              <el-row style="width:100%"/>
-              <image-upload
-                  :value.sync="item.avatarOrBusinessCard"
-                  :limit="1"
-                  :isShowTip="false"
-              >
+              <el-row style="width:100%" />
+              <image-upload :value.sync="item.avatarOrBusinessCard" :limit="1" :isShowTip="false">
               </image-upload>
             </el-form-item>
             <el-form-item label="联系人备注">
@@ -119,7 +113,7 @@
         <div>
           <div class="collapse flex-middle flex-center fs-12 mt-10 pointer" @click="onCollapsed(item.contactId)">
             {{ item.show ? '收起' : '展开全部(选填)' }}
-            <i class="ml-6" :class="item.show ? 'el-icon-arrow-up':'el-icon-arrow-down'"></i>
+            <i class="ml-6" :class="item.show ? 'el-icon-arrow-up' : 'el-icon-arrow-down'"></i>
           </div>
         </div>
       </div>
@@ -133,9 +127,9 @@
 </template>
 
 <script>
-import {generatePhone} from "@/utils/tools";
-import {deepClone} from "@/utils";
-import {platformOption, rankOption, sexRadio} from "@/constant/customer/ContactCard";
+import { generatePhone } from "@/utils/tools";
+import { deepClone } from "@/utils";
+import { platformOption, rankOption, sexRadio } from "@/constant/customer/ContactCard";
 
 const addConstruct = {
   contactId: +new Date(),
@@ -176,11 +170,11 @@ export default {
   data() {
     return {
       formList: [
-        {...deepClone(addConstruct)}
+        { ...deepClone(addConstruct) }
       ],
       contactRule: {
         nickName: [
-          {required: true, message: '请填写昵称', trigger: 'blur'}
+          { required: true, message: '请填写昵称', trigger: 'blur' }
         ]
       },
       phonePrefixList: generatePhone(),
@@ -197,8 +191,19 @@ export default {
           this.formList = list.map(val => {
             val.show = false
             val.contactId = val.id
+            // 添加id
+            val.phone = val.phone?.map((p, index) => {
+              p.id = index
+              return p
+            }) || []
+            // 添加id
+            val.socialPlatform = val.socialPlatform?.map((s, index) => {
+              s.id = index
+              return s
+            }) || []
             return val
           })
+          console.log(this.formList);
         }
       },
       deep: true,
@@ -215,7 +220,7 @@ export default {
       })
     },
     onAddContactForm() {
-      this.formList.push({...deepClone(addConstruct), contactId: +new Date()})
+      this.formList.push({ ...deepClone(addConstruct), contactId: +new Date() })
     },
     onDelContactForm(contactId) {
       this.formList = this.formList.filter(val => val.contactId !== contactId)
@@ -244,11 +249,14 @@ export default {
       })
     },
     // 删除平台或联系人
-    onReduce(id, type) {
+    onReduce(contactId, id, type) {
       this.formList.map(val => {
-        val[type] = val[type].filter(val => val.id !== id)
+        if (val.id === contactId) {
+          val[type] = val[type].filter(val => val.id !== id)
+        }
         return val
       })
+      console.log(this.formList);
     },
     getInnerData() {
       let valid = []
